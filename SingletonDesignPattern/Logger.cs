57cc4905e91bbbeb94c02d88b944ed55558e043c
emrecanayar <emrecan.ayar@technoface.com.tr>
2024-07-27 18:25:05 +0300

    Singleton Design Pattern Code

---
+++ SingletonDesignPattern/Logger.cs
@@ -0,0 +1,27 @@
+﻿namespace SingletonDesignPattern
+{
+    public class Logger
+    {
+        private static readonly Lazy<Logger> _instance = new Lazy<Logger>(() => new Logger());
+
+        //Logger sınıfının yapıcısı(constructor) özel olarak işaretlenmiştir, bu nedenle sınıfın dışından yeni örnek oluşturulamaz.Bu, Singleton tasarım deseninin önemli bir parçasıdır.
+        private Logger()
+        {
+        }
+
+        // Lazy<T> sınıfı, Logger sınıfının örneğinin yalnızca gerekli olduğunda oluşturulmasını sağlar.Bu, örneğin kullanılmadığı durumlarda gereksiz yere bellek kullanılmasını önler.
+        // Lazy<T> sınıfı, varsayılan olarak thread-safe'dir, bu da aynı anda birden fazla iş parçacığı tarafından erişilmesi durumunda dahi yalnızca bir örnek oluşturulacağını garanti eder.Bu örneğin kullanımı, örneğin yalnızca bir kez oluşturulmasını sağlar.
+        public static Logger Instance
+        {
+            get
+            {
+                return _instance.Value;
+            }
+        }
+
+        public void Log(string message)
+        {
+            Console.WriteLine($"Log: {message}");
+        }
+    }
+}
---
+++ SingletonDesignPattern/Program.cs
@@ -0,0 +1,13 @@
+﻿using SingletonDesignPattern;
+
+Logger logger1 = Logger.Instance;
+Logger logger2 = Logger.Instance;
+
+
+logger1.Log("This is the first log message.");
+logger2.Log("This is the second log message.");
+
+if (ReferenceEquals(logger1, logger2))
+{
+    Console.WriteLine("Logger1 and Logger2 are the same instance.");
+}
